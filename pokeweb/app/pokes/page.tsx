4cd52abcfbebb83de2b60084dd9e65535b6b307f
zincronito <zincronito@gmail.com>
2026-02-04 22:37:01 -0600

    mensaje de bienvenida

---
+++ pokeweb/app/pokes/page.tsx
@@ -0,0 +1,9 @@
+export default function PokesPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full text-gray-500">
+      <h2 className="text-2xl font-bold">Bienvenido a la PokeWeb</h2>
+      <p>Selecciona un pokémon.</p>
+      <span className="text-4xl mt-4"></span>
+    </div>
+  );
+}
